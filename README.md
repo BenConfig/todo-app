33c5581cd37d6ecd4bc8b70a2d472aede76b8b76
Ben King <btking1990@googlemail.com>
2022-01-16 21:16:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - Todo App Solution
+
+This is a solution to the [Todo App Challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
+
+## Screenshot
+
+### Mobile
+![](screenshot-mobile.png)
+
+### Desktop
+![](screenshot-mobile.png)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JS
+
+### What I learned
+
+I'm pleased to report that I managed to accomplish the bonus challenge, which was to add drag and drop functionality so that the user can reorder the items in the list. I did however rely heavily on a video tutorial, therefore much of the code is copied and I have yet to understand exactly how each line of code works. I plan to revisit the video and others to gain a better understanding.
+
+I had the opportunity to use `align-items: baseline` which is a property value I've always been aware of but never found a situation to apply it. I used it on a flex container (the `.todo-footer`) so that the child elements, which have different vertical padding values, line up together along the baseline of the text. I will keep an eye out for more situtations where this property value can be applied; I think I may have missed a few.
+
+I was forced to learn about **Event Delegation** in this project because of the elements that are dynamically added when the user creates a to-do item. I added the event listener to the parent `<ol>` element as this is always present on the page, then I detect which element is being clicked by seeing if the clicked element has a specific class:
+```js
+TODO_LIST.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-delete-item')) {
+       // More code
+    }
+})
+```
+
+This project also helped me understand how to create elements, attach classess and attributes, etc., and then append the elements to a parent. I'm sure jQuery would allow much simpler and cleaner code for this task, but I think practicing this with Vanilla JavaScript has been a very beneficial exercise for me.
+
+## Continued development
+
+The intermediate challenges on Frontend Mentor are definitely pushing my abilities, so I plan on working my way through some more of them. Meanwhile I am studying a course on Udemy and am about to start the module about AJAX and APIs which I think I'll need to know for some of the more advanced Frontend Mentor projects.
+
+## Useful resources
+
+- [How To Build Sortable Drag & Drop With Vanilla Javascript](https://www.youtube.com/watch?v=jfYWwQrtzzY) - This is the video that guided me through implementing drag and drop functionality on the page. It's a good video, however it's very fast and I feel that a few concepts could have been explained more.
---
+++ main.js
@@ -0,0 +1,265 @@
+/* ------------------------------------------------------ */
+/*                    Toggle Dark Mode                    */
+/* ------------------------------------------------------ */
+const TOGGLE = document.querySelector('.btn-toggle-dark-mode');
+const STATUS = document.getElementById('status');
+
+TOGGLE.addEventListener('click', function() {
+    if (document.documentElement.classList.contains('dark')){
+        document.documentElement.classList.remove('dark');
+        STATUS.innerText = 'Dark mode is off.';
+    }
+    else {
+        document.documentElement.classList.add('dark');
+        STATUS.innerText = 'Dark mode is on.';
+    }
+});
+
+/* ------------------------------------------------------ */
+/*                 Todo Item Functionality                */
+/* ------------------------------------------------------ */
+let isAnimating = false;
+
+const TODO_LIST = document.querySelector('.list');
+const ITEMS_LEFT = document.querySelector('.items-left');
+
+function updateItemsLeftNum() {
+    let completedItems = 0;
+    for (let item of TODO_LIST.children) {
+        if (item.classList.contains('completed')) completedItems++;
+    }
+    ITEMS_LEFT.innerText = TODO_LIST.children.length - completedItems + ' items left';
+}
+
+// Initial call
+updateItemsLeftNum();
+
+/* ----------------- Create a Todo Item ----------------- */
+const CREATE_ITEM_INPUT = document.querySelector('.create-todo-input');
+const CREATE_ITEM_BTN = document.querySelector('.circle-create-item');
+
+function createItem() {
+    const itemText = CREATE_ITEM_INPUT.value.trim();
+    if (!itemText) return;
+
+    // Make <li>
+    const item = document.createElement('li');
+    item.classList.add('item', 'reveal');
+    item.setAttribute('draggable', 'true');
+
+    // Make 'complete' <button>
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('circle', 'circle-complete-item');
+    completeBtn.setAttribute('type', 'button');
+    completeBtn.setAttribute('aria-label', 'Complete item');
+
+    // Make <span> for item task
+    const itemTextContainer = document.createElement('span');
+    itemTextContainer.classList.add('task');
+    itemTextContainer.innerText = itemText;
+
+    // Make <span> for item completion status
+    const completionStatus = document.createElement('span');
+    completionStatus.classList.add('sr-only');
+    completionStatus.setAttribute('aria-live', 'polite');
+    completionStatus.innerText = 'Task not yet completed';
+    
+    // Make 'delete' <button>
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn-delete-item');
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.setAttribute('aria-label', 'Delete item');
+
+    item.append(completeBtn, itemTextContainer, completionStatus, deleteBtn);
+
+    TODO_LIST.append(item);
+    
+    CREATE_ITEM_INPUT.value = '';
+
+    setTimeout(() => item.classList.remove('reveal'));
+
+    updateItemsLeftNum();
+};
+
+CREATE_ITEM_INPUT.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') createItem();
+});
+CREATE_ITEM_BTN.addEventListener('click', createItem);
+
+/* ------------ Only Show Selected Todo Items ----------- */
+const SHOW_BTNS = document.querySelectorAll('[type="radio"]');
+
+function shownItemsFilter() {
+    if (SHOW_BTNS[0].checked) { // The 'All' radio button
+        for (let item of TODO_LIST.children) {
+            if (item.classList.contains('hide')) {
+                item.classList.remove('hide');
+            }
+        }
+    }
+
+    else if (SHOW_BTNS[1].checked) { // The 'Active' radio button
+        for (let item of TODO_LIST.children) {
+            if (item.classList.contains('completed')) {
+                item.classList.add('hide');
+            }
+            else if (item.classList.contains('hide')) {
+                item.classList.remove('hide');
+            }
+        }
+    }
+
+    else if (SHOW_BTNS[2].checked) { // The 'Completed' radio button
+        for (let item of TODO_LIST.children) {
+            if (!item.classList.contains('completed')) {
+                item.classList.add('hide');
+            }
+            else if (item.classList.contains('hide')) {
+                item.classList.remove('hide');
+            }
+        }
+    }
+}
+
+SHOW_BTNS.forEach(btn => btn.addEventListener('change', shownItemsFilter));
+
+/* ---------------- Complete a Todo Item ---------------- */
+TODO_LIST.addEventListener('click', function(e)  {
+    if (e.target.classList.contains('circle-complete-item')) {
+        
+        let PARENT_LI = e.target.parentElement;
+
+        if (!PARENT_LI.classList.contains('completed')) {
+            if (isAnimating) return;
+
+            isAnimating = true;
+
+            PARENT_LI.classList.add('completed');
+            setTimeout(function() {
+                PARENT_LI.classList.add('fade');
+                isAnimating = false;
+                
+                PARENT_LI.querySelector('[aria-live="polite"]').innerText = 'Task completed';
+
+                updateItemsLeftNum();
+                shownItemsFilter();
+            }, 250);
+        }
+        else {
+
+            if (isAnimating) return;
+
+            PARENT_LI.classList.remove('completed');
+            PARENT_LI.classList.remove('fade');
+
+            PARENT_LI.querySelector('[aria-live="polite"]').innerText = 'Task not yet completed';
+
+            updateItemsLeftNum();
+            shownItemsFilter();
+        }
+    }
+});
+
+/* ----------------- Delete a Todo Item ----------------- */
+TODO_LIST.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-delete-item')) {
+        if (isAnimating) return;
+
+        isAnimating = true;
+
+        let PARENT_LI = e.target.parentElement;
+
+        PARENT_LI.classList.add('delete');
+        
+        setTimeout(function() {
+            PARENT_LI.remove();
+        }, 250);
+
+        setTimeout(function() {
+            isAnimating = false;
+            updateItemsLeftNum();
+        }, 250);
+        
+    }
+})
+
+/* ------------- Clear Completed Todo Items ------------- */
+const CLEAR_COMPLETED = document.querySelector('.btn-clear-completed');
+
+CLEAR_COMPLETED.addEventListener('click', function() {
+
+    let deleteCount = 0;
+
+    for (let item of TODO_LIST.children) {
+        if (item.classList.contains('completed')) {
+            
+            deleteCount++;
+
+            item.classList.add('delete');
+            
+            setTimeout(function() {
+                item.remove();
+            }, 250);
+        }
+    }
+    updateItemsLeftNum(deleteCount);
+});
+
+/* ------------- Drag and Drop Functionality ------------ */
+let draggables = [];
+
+TODO_LIST.addEventListener('mousedown', function(e) {
+    if (e.target.classList.contains('item') || e.target.classList.contains('task')) {
+        draggables = [...TODO_LIST.children];
+    }
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add('dragging');
+        })
+    
+        draggable.addEventListener('dragend', () => {
+            draggable.classList.remove('dragging');
+        })
+    })
+})
+
+TODO_LIST.addEventListener('dragover', e => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(TODO_LIST, e.clientY);
+    
+    const draggable = document.querySelector('.dragging');
+    if (afterElement == null) {
+        TODO_LIST.appendChild(draggable);
+    }
+    else {
+        TODO_LIST.insertBefore(draggable, afterElement)
+    }
+})
+
+function getDragAfterElement(TODO_LIST, yPosition) {
+    const draggableElements = [...TODO_LIST.querySelectorAll('.item:not(.dragging)')]
+    
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = yPosition - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        }
+        else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+/* ------------------------------------------------------ */
+/*         Stop Animations During Window Resizing         */
+/* ------------------------------------------------------ */
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
